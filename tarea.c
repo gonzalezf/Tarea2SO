cc86ab33af2c1e01cc07e4e36a8f5fa616b7a029
root <=>
2014-09-17 19:26:39 -0300

    iedjidejedi

--- tarea.c
+++ tarea.c
@@ -1,14 +1,41 @@
-/* Felipe Morales
- * Felipe González */
- 
- 
- 
- 
- 
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+ /* crear subprocesos mediante fork
+  * cada subproceso entregar informacion sobre estado del sistema y enviar resultados al padre mediante
+  * memoria compartida
+  * 
+  * Registrar los resultados, PID del proceso que envia el mensaje y el contenido en el archivo log.txt
+  * Cuando hijo termine escribir PID subproveso finalizado
+  * CUando padre termine escribir PID Programa finalizado
+  * 
+  * Cada hijo analiza el sistema n veces, el usuario determina n.
+  * Cada hijo toma el tiempo de espera entre cada analisis realizado
+  * 
+  * Padre creare 3 subprocesos que funcionen en paralelo
+  *el primero de ellos entrega tiempo en segundos que lleva encendido el sistema
+  * el segundo entrega cantidad de ram disponible sobre la total
+  * tercero indica la cantidad de procesos en ejecucion
+  * */
  
  int main(){
+	 pid_t pid;
+	 pid = fork();
 	 
-	 
+	 if(pid == -1) ;
+	 {
+	 printf("Error al crear proceso hijo \n");
 	 return 0;
-	 
 	 }
+	 if(pid == 0){ /* ejecutar instrucciones del hijo*/
+	
+	 printf("Soy el hijo \n");
+	 return 0;
+	}
+	 else if(pid >0) /* proceso padre, retorna el PID del hijo*/
+	 {
+		printf("Proceso padre, Pid del hijo es %d \n",pid);
+		 return 0;
+		}
+	 	 
+ }
